922971c110d30dc15ab9d64d9c68173bf4eab206
Agustin Compean <agustincompean@Agustins-MacBook-Pro.local>
2022-11-16 23:11:15 -0600

    components folder added

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar() {
+    return (
+        <>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+export default Navbar
